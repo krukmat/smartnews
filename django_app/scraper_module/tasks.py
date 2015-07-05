dbabdeba5a01c357bfa1948a3363fa5389d33522
Matias Kruk <kruk.matias@gmail.com>
2015-07-05 16:39:53 -0300

    canvas to connect with nlp processing

--- django_app/scraper_module/tasks.py
+++ django_app/scraper_module/tasks.py
@@ -1,8 +1,21 @@
+from celery.canvas import group
 from djcelery.app import app
-from news_crawler.news_crawler.utils import run_spiders
+from news_crawler.news_crawler.utils import run_spider, spiders
 
+@app.task
+def scrape_site(spider):
+    try:
+        run_spider(spider)
+    except Exception:
+        return False
+    return True
 
 @app.task
 def scrape_news():
-    run_spiders()
+    scraped_news = group(scrape_site.s(spider) for spider in spiders)
+    (scraped_news | compute_nlp.s())()
+    return True
+
+@app.task
+def compute_nlp(results):
     return True
